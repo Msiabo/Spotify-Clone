472fed34a997f030bfba76dc085a5c6f9f39f1ac
Siyabonga Mhlongo <mhlongoquincy@gmail.com>
2025-08-17 18:08:08 +0200

    initial commit

--- spotify-clone/src/components/Sidebar.jsx
+++ spotify-clone/src/components/Sidebar.jsx
@@ -1,23 +1,20 @@
 import React from 'react'
-import { assets } from '../assets/assets'
-
-// Destructure the images from assets
-const { home, favourites, history } = assets
+import { FiHome, FiHeart, FiClock } from 'react-icons/fi'
 
 const Sidebar = () => {
   return (
     <div className='w-[25%] text-white h-screen p-2 gap-2 flex-col hidden lg:flex bg-black'>
       <div className='bg-[#121212] h-[15%] rounded flex-col flex justify-around'>
         <div className='flex items-center gap-3 pl-8 cursor-pointer'>
-          <img className='w-6' src={home} alt='Home' />
+          <FiHome className='w-6 h-6' />
           <p className='font-bold'>Home</p>
         </div>
         <div className='flex items-center gap-3 pl-8 cursor-pointer'>
-          <img className='w-6' src={favourites} alt='Favourites' />
+          <FiHeart className='w-6 h-6' />
           <p className='font-bold'>Favourites</p>
         </div>
         <div className='flex items-center gap-3 pl-8 cursor-pointer'>
-          <img className='w-6' src={history} alt='History' />
+          <FiClock className='w-6 h-6' />
           <p className='font-bold'>History</p>
         </div>
       </div>
